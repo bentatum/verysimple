c75f9482fe0955c3276b9cf8cb1ed8f10200b04b
Ben Tatum <mail@benjtatum.com>
2023-10-16 16:28:01 -0400

    feat: chip

---
+++ packages/react/src/Chip/Chip.test.tsx
@@ -0,0 +1,50 @@
+import { render } from "@testing-library/react";
+import { createRef } from "react";
+import Chip from "./Chip";
+
+describe("Chip", () => {
+  test("snapshot", () => {
+    const { container } = render(<Chip>test</Chip>);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+  test("override padding", () => {
+    const { container } = render(<Chip className="p-10">test</Chip>);
+    expect(container.firstChild).toHaveClass("p-10");
+    expect(container.firstChild).not.toHaveClass("px-7", "py-2");
+  });
+  test("default background", () => {
+    const { container } = render(<Chip>test</Chip>);
+    expect(container.firstChild).toHaveClass("st-bg-primary");
+  });
+  test("override background", () => {
+    const { container } = render(<Chip className="bg-red-500">test</Chip>);
+    expect(container.firstChild).toHaveClass("bg-red-500");
+    expect(container.firstChild).not.toHaveClass("st-bg-primary");
+  });
+  test("default text size", () => {
+    const { container } = render(<Chip>test</Chip>);
+    expect(container.firstChild).toHaveClass("text-base");
+  });
+  test("override text size", () => {
+    const { container } = render(<Chip className="text-base">test</Chip>);
+    expect(container.firstChild).toHaveClass("text-base");
+    expect(container.firstChild).not.toHaveClass("text-sm");
+  });
+  test("default border radius", () => {
+    const { container } = render(<Chip>test</Chip>);
+    expect(container.firstChild).toHaveClass("rounded-full");
+  });
+  test("override border radius", () => {
+    const { container } = render(<Chip className="rounded-none">test</Chip>);
+    expect(container.firstChild).toHaveClass("rounded-none");
+    expect(container.firstChild).not.toHaveClass("rounded-full");
+  });
+  it("forwards the ref", () => {
+    const ref = createRef<HTMLDivElement>();
+    render(<Chip ref={ref}>test</Chip>);
+    expect(ref.current).toBeInTheDocument();
+  });
+  it("has a display name", () => {
+    expect(Chip.displayName).toBe("Chip");
+  });
+});
---
+++ packages/react/src/Chip/Chip.tsx
@@ -0,0 +1,36 @@
+import { fieldPaddingClassNames } from "@/helpers/fieldPaddingClassNames";
+import { fieldSizeClassNames } from "@/helpers/fieldSizeClassNames";
+import { ChipSize } from "@/types";
+import classNames from "classnames";
+import { ComponentPropsWithRef, forwardRef } from "react";
+
+export interface ChipProps extends ComponentPropsWithRef<"div"> {
+  size?: ChipSize;
+}
+
+export const Chip = forwardRef<HTMLDivElement, ChipProps>(
+  ({ className = "", size = "md", ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={classNames(
+          "inline-flex items-center",
+          fieldSizeClassNames(size),
+          fieldPaddingClassNames(size, className),
+          {
+            "font-semibold": !className.includes("font-"),
+            "st-bg-primary": !className.includes("bg-"),
+            "rounded-full": !className.includes("rounded-"),
+            "text-black dark:text-white": !className.includes("text-"),
+          },
+          className
+        )}
+        {...props}
+      />
+    );
+  }
+);
+
+Chip.displayName = "Chip";
+
+export default Chip;
