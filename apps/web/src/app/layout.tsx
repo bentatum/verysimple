ae26c9fce7987317e83940ae6485b11725545619
Ben Tatum <mail@benjtatum.com>
2023-12-18 14:24:25 -0500

    feat: og image

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,15 +1,37 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Layout from "@/components/Layout/Layout";
+import Layout from "@/components/Layout";
 import classNames from "classnames";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const title = "VerySimple UI Components";
+const description =
+  "Simple, customizable and accessible UI components for React and Tailwind projects.";
+
 export const metadata: Metadata = {
-  title: "VerySimple UI Components",
-  description:
-    "Simple, customizable and accessible UI components for React and Tailwind projects.",
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    url: "https://verysimple.dev",
+    images: [
+      "https://res.cloudinary.com/ben-tatum/image/upload/v1702927298/VerySimple_OG.png",
+    ],
+  },
+  twitter: {
+    title,
+    description,
+    // site: "@benjtatum",
+    // siteId: "benjtatum",
+    // creator: "@benjtatum",
+    // creatorId: "benjtatum",
+    images: [
+      "https://res.cloudinary.com/ben-tatum/image/upload/v1702927298/VerySimple_OG.png",
+    ],
+  },
 };
 
 export default function RootLayout({
