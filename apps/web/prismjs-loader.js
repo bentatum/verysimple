63715790a9c58e69e26e47c420ef39c2f548d927
Ben Tatum <mail@benjtatum.com>
2024-02-02 14:12:40 -0500

    fix: remove accidentally added files and dirs and refresh yarn lock

--- apps/web/prismjs-loader.js
+++
@@ -1,9 +0,0 @@
-const Prism = require('prismjs');
-const loadLanguages = require('prismjs/components/');
-
-loadLanguages(['html']);
-
-module.exports = function (source) {
-  const code = Prism.highlight(source, Prism.languages.html, 'html');
-  return `export default ${JSON.stringify(code)}`;
-};
