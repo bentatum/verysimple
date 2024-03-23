f788e6052965ed198e4293174b835a7e107b6ed7
Ben Tatum <mail@benjtatum.com>
2024-03-23 13:55:12 -0400

    fix: lint

--- packages/react/src/TextButton/TextButton.tsx
+++ packages/react/src/TextButton/TextButton.tsx
@@ -9,23 +9,36 @@ import {
   fieldBorderRadiusClassNames,
 } from "@/helpers";
 import Ripple from "@/Ripple";
-import { cva } from "class-variance-authority";
+import { ButtonColor } from "..";
 
-export interface TextButtonProps extends ButtonBaseProps {}
+export interface TextButtonProps extends ButtonBaseProps {
+  color?: ButtonColor;
+}
 
 export const TextButton = forwardRef<HTMLButtonElement, TextButtonProps>(
   (
-    { className = "", size = "md", color, children, onClick, ...props },
+    { className = "", size = "md", color = "neutral", children, onClick, ...props },
     ref
   ) => {
     const handleClick = (e: any) => {
+      console.log("TextButton clicked");
       if (onClick) {
         onClick(e);
       }
     };
 
+    const hasColorOverride = /text-/.test(className);
+
+    const colors = {
+      primary:
+        "text-primary-500 hover:bg-primary-300 active:bg-primary-300 focus:bg-primary-300",
+      neutral: "hover:bg-gray-300 active:bg-gray-300 focus:bg-gray-300",
+      destructive:
+        "text-red-500 hover:bg-red-300 active:bg-red-300 focus:bg-red-300",
+    };
+
     return (
-      <Ripple >
+      <Ripple>
         <ButtonBase
           {...props}
           size={size}
@@ -36,23 +49,12 @@ export const TextButton = forwardRef<HTMLButtonElement, TextButtonProps>(
             "relative overflow-auto",
             "transition-colors duration-200 ease-in-out",
             "hover:bg-opacity-20 active:bg-opacity-20 focus:bg-opacity-20",
-            "",
             buttonTextClassNames(className),
             fieldPaddingClassNames(size, className),
             buttonShadowClassNames("text", className),
             fieldSizeClassNames(size, className),
             fieldBorderRadiusClassNames(className),
-            cva([], {
-              variants: {
-                color: !/text-/.test(className)
-                  ? {
-                      primary: "text-primary-500 hover:bg-primary-300 active:bg-primary-300 focus:bg-primary-300",
-                      neutral: "hover:bg-gray-300 active:bg-gray-300 focus:bg-gray-300",
-                      destructive: "text-red-500 hover:bg-red-300 active:bg-red-300 focus:bg-red-300",
-                    }
-                  : {},
-              },
-            })({ color }),
+            !hasColorOverride && colors[color],
             className
           )}
         >
