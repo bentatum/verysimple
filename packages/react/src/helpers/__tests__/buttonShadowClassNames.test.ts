32551a24ae7f1c53512a67eb973b06499659bd34
Benjamin Tatum <mail@benjtatum.com>
2024-10-03 14:48:41 -0400

    fix(buttons): use default shadow classnames

--- packages/react/src/helpers/__tests__/buttonShadowClassNames.test.ts
+++ packages/react/src/helpers/__tests__/buttonShadowClassNames.test.ts
@@ -3,13 +3,13 @@ import { buttonShadowClassNames } from "../buttonShadowClassNames";
 describe("buttonShadowClassNames", () => {
   it("should return the default", () => {
     expect(buttonShadowClassNames("filled", "not-an-override")).toContain(
-      "shadow-sm"
+      "shadow"
     );
   });
   it("should be overridable", () => {
     expect(buttonShadowClassNames("filled", "shadow-none")).toBe("");
   });
   it("doesn't fail if the className is undefined", () => {
-    expect(buttonShadowClassNames("filled")).toContain("shadow-sm");
+    expect(buttonShadowClassNames("filled")).toContain("shadow");
   });
 });
