dc89b7aad95d9c46dd88d1315c9f45025d105712
Benjamin Tatum <mail@benjtatum.com>
2024-10-11 13:52:25 -0400

    feat: dialog panel props

--- packages/react/src/Dialog/Dialog.test.tsx
+++ packages/react/src/Dialog/Dialog.test.tsx
@@ -34,4 +34,24 @@ describe("Dialog", () => {
   it("has a display name", () => {
     expect(Dialog.displayName).toBe("Dialog");
   });
-});
+
+  it("applies panelProps correctly", () => {
+    const customClassName = "custom-class";
+    render(
+      // @ts-expect-error - it's a test
+      <Dialog open panelProps={{ className: customClassName, 'data-testid': 'panel' }} onClose={() => {}}>
+        test
+      </Dialog>
+    );
+    const dialogPanel = screen.getByTestId("panel");
+    expect(dialogPanel).toHaveClass(customClassName);
+  });
+
+  it('can override the default max-w-md class', () => {
+    // @ts-expect-error - it's a test
+    render(<Dialog open panelProps={{ className: 'max-w-2xl', 'data-testid': 'panel' }} onClose={() => {}}>test</Dialog>);
+    const dialogPanel = screen.getByTestId("panel");
+    expect(dialogPanel).toHaveClass('max-w-2xl');
+    expect(dialogPanel).not.toHaveClass('max-w-md');
+  });
+}); 
